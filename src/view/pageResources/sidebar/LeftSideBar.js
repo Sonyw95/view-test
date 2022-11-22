d6b74476ed6ed9a205a31e368889db9efbfb5e92
sonyw32 <sonyw32@naver.com>
2022-11-22 18:10:38 +0900

    commit;

--- src/view/pageResources/sidebar/LeftSideBar.js
+++ src/view/pageResources/sidebar/LeftSideBar.js
@@ -14,16 +14,16 @@ const LeftSideBar = () =>{
             <ul className={"menu small"}>
 
                 { menuList.map( (x) => (
-                <li className={"menu-item " + x[2]}>
-                    <Link to={"/"} className={"menu-item-link"}>
-                        <svg className="menu-item-link-icon icon-newsfeed ">
-                            <use href={x[1]}/>
-                        </svg>
-                        <div className={"menu-tooltip"}>
-                            <p className="menu-tooltip-text">{x[0]}</p>
-                        </div>
-                    </Link>
-                </li>
+                    <li className={"menu-item " + x[2]}>
+                        <Link to={"/"} className={"menu-item-link"}>
+                            <svg className="menu-item-link-icon icon-newsfeed ">
+                                <use href={x[1]}/>
+                            </svg>
+                            <div className={"menu-tooltip"}>
+                                <p className="menu-tooltip-text">{x[0]}</p>
+                            </div>
+                        </Link>
+                    </li>
                 ))}
 
             </ul>
